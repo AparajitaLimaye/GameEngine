83e883ef50ce5ecc566f7be509cad3bb7203f0d4
Aparajita Limaye <alimaye2@sundevils.asu.edu>
2023-11-14 17:31:31 -0700

    Set up the GUI Element class

--- GameEngine/GUI/GUIElement.cs
+++ GameEngine/GUI/GUIElement.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
+//using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CPI311.GameEngine.GUI
 {
-    internal class GUIElement
+    public class GUIElement
     {
+        //Properties
+        public delegate void EventHandler(GUIElement sender);
+        public event EventHandler Action;
+        protected void OnAction()
+        {
+            if (Action != null) Action(this);// Any method is not specified yet
+        }
+        public Rectangle Bounds { get; set; }
+        public Texture2D Texture { get; set; }
+        public String Text { get; set; }
+        public bool Selected { get; set; }
+
+        //Update
+        public virtual void Update()
+        {
+            //keepin it blank
+        }
+
+        //Draw
+        public virtual void Draw(SpriteBatch spriteBatch, SpriteFont font)
+        {
+            if(Texture != null)
+            {
+                spriteBatch.Draw(Texture, Bounds, Selected ? Color.Yellow : Color.White);
+            }
+        }
     }
 }
