3960b0cbf88978a9f5d3cc8fd3fc0245efc40351
Aparajita Limaye <alimaye2@sundevils.asu.edu>
2023-11-17 15:26:24 -0700

    Added in the lab 9 node path

--- Assignment5/Assignment5.cs
+++ Assignment5/Assignment5.cs
@@ -3,6 +3,10 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using CPI311.GameEngine;
 using Lab02;
+using System;
+using SharpDX.WIC;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Assignment5
 {
@@ -11,9 +15,19 @@ namespace Assignment5
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        TerrainRenderer terrain;
         Camera camera;
+        Random random = new Random();
+        const int Size = 20;
+
+        //Developing Maze Terrain : Lab 10
+        TerrainRenderer terrain;
         Effect effect;
+
+        //Create grid and search algorithm: Lab 9
+        AStarSearch search;
+        List<Vector3> path;
+        Model cube;
+        Model sphere;
 
         public Assignment5()
         {
@@ -30,6 +44,35 @@ namespace Assignment5
             InputManager.Initialize();
             ScreenManager.Initialize(_graphics);
 
+            //**** Camera
+            camera = new Camera();
+            camera.Transform = new Transform();
+            //Top Down View
+            camera.Transform.LocalPosition = Vector3.Backward * 15 + Vector3.Right * 3 + Vector3.Up * 55;
+            camera.Transform.Rotate(Vector3.Left, 20.3f);
+
+            //First Person View
+            //camera.Transform.LocalPosition = Vector3.Backward * 5 + Vector3.Right*3 + Vector3.Up * 5;
+
+            //*** Grid
+            search = new AStarSearch(Size, Size); //size of grid
+            foreach (AStarNode node in search.Nodes)
+                if (random.NextDouble() < 0.2)
+                    search.Nodes[random.Next(Size), random.Next(Size)].Passable = false;
+            search.Start = search.Nodes[0, 0];
+            search.Start.Passable = true;
+            search.End = search.Nodes[Size - 1, Size - 1];
+            search.End.Passable = true;
+
+            search.Search(); // a search is made here
+            path = new List<Vector3>();
+            AStarNode current = search.End;
+            while (current != null)
+            {
+                path.Insert(0, current.Position);
+                current = current.Parent;
+            }
+
             base.Initialize();
         }
 
@@ -37,6 +80,7 @@ namespace Assignment5
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            //*** Terrain Maze
             terrain = new TerrainRenderer(
                 Content.Load<Texture2D>("mazeH"),
                 Vector2.One * 100, Vector2.One * 200);
@@ -49,9 +93,8 @@ namespace Assignment5
             effect.Parameters["SpecularColor"].SetValue(new Vector3(0.5f, 0.5f, 0.5f));
             effect.Parameters["Shininess"].SetValue(20f);
 
-            camera = new Camera();
-            camera.Transform = new Transform();
-            camera.Transform.LocalPosition = Vector3.Backward * 5 + Vector3.Right * 3 + Vector3.Up * 5;
+            cube = Content.Load<Model>("cube");
+            sphere = Content.Load<Model>("Sphere");
         }
 
         protected override void Update(GameTime gameTime)
@@ -81,6 +124,14 @@ namespace Assignment5
                 pass.Apply();
                 terrain.Draw();
             }
+
+            foreach(AStarNode node in search.Nodes)
+                if(!node.Passable)
+                    cube.Draw(Matrix.CreateScale(0.5f, 0.05f, 0.5f) *
+                        Matrix.CreateTranslation(node.Position), camera.View, camera.Projection);
+            foreach(Vector3 position in path)
+                sphere.Draw(Matrix.CreateScale(0.1f, 0.1f, 0.1f) *
+                    Matrix.CreateTranslation(position), camera.View, camera.Projection);
 
             base.Draw(gameTime);
         }
